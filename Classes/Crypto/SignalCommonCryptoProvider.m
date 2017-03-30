1cd35f504f9485aaf6668c2122429c643017ace3
Chris Ballinger <chrisballinger@gmail.com>
2017-03-30 13:57:33 -0700

    Update to libsignal-protocol-c 2.3.1

--- Classes/Crypto/SignalCommonCryptoProvider.m
+++ Classes/Crypto/SignalCommonCryptoProvider.m
@@ -13,29 +13,53 @@
 static int random_func(uint8_t *data, size_t len, void *user_data) {
     CCRNGStatus result = CCRandomGenerateBytes(data, len);
     if (result != kCCSuccess) {
-        return -1;
+        return SG_ERR_INVAL;
     }
-    return 0;
+    return SG_SUCCESS;
 }
 
 static int hmac_sha256_init_func(void **hmac_context, const uint8_t *key, size_t key_len, void *user_data) {
+    NSCParameterAssert(hmac_context);
+    NSCParameterAssert(key);
+    if (!hmac_context || !key) {
+        return SG_ERR_INVAL;
+    }
     CCHmacContext *context = malloc(sizeof(CCHmacContext));
+    if (!context) {
+        return SG_ERR_NOMEM;
+    }
     CCHmacInit(context, kCCHmacAlgSHA256, key, key_len);
     *hmac_context = context;
-    return 0;
+    return SG_SUCCESS;
 }
 
 static int hmac_sha256_update_func(void *hmac_context, const uint8_t *data, size_t data_len, void *user_data) {
+    NSCParameterAssert(hmac_context);
+    NSCParameterAssert(data);
+    if (!hmac_context || !data) {
+        return SG_ERR_INVAL;
+    }
     CCHmacUpdate(hmac_context, data, data_len);
-    return 0;
+    return SG_SUCCESS;
 }
 
 static int hmac_sha256_final_func(void *hmac_context, signal_buffer **output, void *user_data) {
+    NSCParameterAssert(hmac_context);
+    NSCParameterAssert(output);
+    if (!hmac_context || !output) {
+        return SG_ERR_INVAL;
+    }
     NSMutableData *mutableData = [NSMutableData dataWithLength:CC_SHA256_DIGEST_LENGTH];
+    if (!mutableData) {
+        return SG_ERR_NOMEM;
+    }
     CCHmacFinal(hmac_context, mutableData.mutableBytes);
     signal_buffer *macOut = signal_buffer_create(mutableData.bytes, mutableData.length);
+    if (!macOut) {
+        return SG_ERR_NOMEM;
+    }
     *output = macOut;
-    return 0;
+    return SG_SUCCESS;
 }
 
 static void hmac_sha256_cleanup_func(void *hmac_context, void *user_data) {
@@ -44,10 +68,67 @@ static void hmac_sha256_cleanup_func(void *hmac_context, void *user_data) {
     }
 }
 
+static int sha512_digest_init_func(void **digest_context, void *user_data) {
+    NSCParameterAssert(digest_context);
+    if (!digest_context) {
+        return SG_ERR_INVAL;
+    }
+    CC_SHA512_CTX *context = malloc(sizeof(CC_SHA512_CTX));
+    if (!context) {
+        return SG_ERR_NOMEM;
+    }
+    CC_SHA512_Init(context);
+    *digest_context = context;
+    return SG_SUCCESS;
+}
+
+static int sha512_digest_update_func(void *digest_context, const uint8_t *data, size_t data_len, void *user_data) {
+    NSCParameterAssert(digest_context);
+    NSCParameterAssert(data);
+    if (!digest_context || !data) {
+        return SG_ERR_INVAL;
+    }
+    CC_SHA512_Update(digest_context, data, (CC_LONG)data_len);
+    return SG_SUCCESS;
+}
+
+static int sha512_digest_final_func(void *digest_context, signal_buffer **output, void *user_data) {
+    NSCParameterAssert(digest_context);
+    NSCParameterAssert(output);
+    if (!digest_context || !output) {
+        return SG_ERR_INVAL;
+    }
+    NSMutableData *mutableData = [NSMutableData dataWithLength:CC_SHA512_DIGEST_LENGTH];
+    if (!mutableData) {
+        return SG_ERR_NOMEM;
+    }
+    CCHmacFinal(mutableData.mutableBytes, digest_context);
+    signal_buffer *digestOut = signal_buffer_create(mutableData.bytes, mutableData.length);
+    if (!digestOut) {
+        return SG_ERR_NOMEM;
+    }
+    *output = digestOut;
+    return SG_SUCCESS;
+}
+
+static void sha512_digest_cleanup_func(void *digest_context, void *user_data) {
+    if (digest_context) {
+        free(digest_context);
+    }
+}
+
+
+
 static int sha512_digest_func(signal_buffer **output, const uint8_t *data, size_t data_len, void *user_data) {
     NSMutableData *mutableData = [NSMutableData dataWithLength:CC_SHA512_DIGEST_LENGTH];
+    if (!mutableData) {
+        return SG_ERR_NOMEM;
+    }
     CC_SHA512(data, (CC_LONG)data_len, mutableData.mutableBytes);
     signal_buffer *digestOut = signal_buffer_create(mutableData.bytes, mutableData.length);
+    if (!digestOut) {
+        return SG_ERR_NOMEM;
+    }
     *output = digestOut;
     return 0;
 }
@@ -73,13 +154,16 @@ static int encrypt_func(signal_buffer **output,
                            void *user_data) {
     // We only support Version 3
     if (cipher != SG_CIPHER_AES_CBC_PKCS5) {
-        return -1;
+        return SG_ERR_INVAL;
     }
     
     size_t outLength;
     NSMutableData *
     cipherData = [NSMutableData dataWithLength:plaintext_len +
                   kCCBlockSizeAES128];
+    if (!cipherData) {
+        return SG_ERR_NOMEM;
+    }
     
     CCCryptorStatus
     result = CCCrypt(kCCEncrypt, // operation
@@ -96,13 +180,15 @@ static int encrypt_func(signal_buffer **output,
     
     if (result == kCCSuccess) {
         cipherData.length = outLength;
-    }
-    else {
-        return -1;
+    } else {
+        return SG_ERR_UNKNOWN;
     }
     signal_buffer *outputBuffer = signal_buffer_create(cipherData.bytes, cipherData.length);
+    if (!outputBuffer) {
+        return SG_ERR_NOMEM;
+    }
     *output = outputBuffer;
-    return 0;
+    return SG_SUCCESS;
 }
 
 /**
@@ -126,13 +212,16 @@ static int decrypt_func(signal_buffer **output,
                     void *user_data) {
     // We only support Version 3
     if (cipher != SG_CIPHER_AES_CBC_PKCS5) {
-        return -1;
+        return SG_ERR_INVAL;
     }
     
     size_t outLength;
     NSMutableData *
     outData = [NSMutableData dataWithLength:ciphertext_len +
                   kCCBlockSizeAES128];
+    if (!outData) {
+        return SG_ERR_NOMEM;
+    }
     
     CCCryptorStatus
     result = CCCrypt(kCCDecrypt, // operation
@@ -149,13 +238,15 @@ static int decrypt_func(signal_buffer **output,
     
     if (result == kCCSuccess) {
         outData.length = outLength;
-    }
-    else {
-        return -1;
+    } else {
+        return SG_ERR_UNKNOWN;
     }
     signal_buffer *outputBuffer = signal_buffer_create(outData.bytes, outData.length);
+    if (!outputBuffer) {
+        return SG_ERR_NOMEM;
+    }
     *output = outputBuffer;
-    return 0;
+    return SG_SUCCESS;
 }
 
 @implementation SignalCommonCryptoProvider
@@ -167,7 +258,10 @@ static int decrypt_func(signal_buffer **output,
     cryptoProvider.hmac_sha256_update_func = hmac_sha256_update_func;
     cryptoProvider.hmac_sha256_final_func = hmac_sha256_final_func;
     cryptoProvider.hmac_sha256_cleanup_func = hmac_sha256_cleanup_func;
-    cryptoProvider.sha512_digest_func = sha512_digest_func;
+    cryptoProvider.sha512_digest_init_func = sha512_digest_init_func;
+    cryptoProvider.sha512_digest_update_func = sha512_digest_update_func;
+    cryptoProvider.sha512_digest_final_func = sha512_digest_final_func;
+    cryptoProvider.sha512_digest_cleanup_func = sha512_digest_cleanup_func;
     cryptoProvider.encrypt_func = encrypt_func;
     cryptoProvider.decrypt_func = decrypt_func;
     cryptoProvider.user_data = (__bridge void *)(self);
--- SignalProtocol-ObjC.podspec
+++ SignalProtocol-ObjC.podspec
@@ -65,5 +65,5 @@ Pod::Spec.new do |s|
 
   s.source_files  = ["Classes/**/*.{h,m}"]
   s.private_header_files = ["Classes/**/*_Internal.h", "Classes/Models/SignalMessage.h"]
-  s.dependency 'SignalProtocolC', '~> 0.1.0'
+  s.dependency 'SignalProtocolC', '~> 2.3.1'
 end
